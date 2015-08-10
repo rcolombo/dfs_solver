4bd27d0fc1abeb41a21dfb03234cdf8fc54d6cb6
Ryan Colombo <rcolombo@cogolabs.com>
2015-08-10 12:42:15 -0400

    Scraper now supports updates

--- scrape/razzball/razzball/spiders/razzball.py
+++ scrape/razzball/razzball/spiders/razzball.py
@@ -10,12 +10,14 @@ import sys
 
 
 class RazzSpider(scrapy.Spider):
-    db_uri = 'postgresql://localhost/dfs'
-    engine = create_engine(db_uri)
     name = 'razzball'
-    #start_urls = ['http://razzball.com/dfsbot-fanduel-hit/']
-    start_urls = ['http://razzball.com/dfsbot-fanduel-pitch/?uid=1505&token=6eef511269ae262c13541abf93953e8e18bd9b7d15b1b9ea74bf4c86c5cb5818&time=1439075726&loadScript=true',
-            'http://razzball.com/dfsbot-fanduel-hit/?uid=1505&token=38c84d51c9e5e3b1a287c39d151ec6c3e6c91881c102655271ec3b67611ea7fd&time=1439085176&loadScript=true']
+
+    def __init__(self, update=False):
+        self.update = update
+        self.db_uri = 'postgresql://localhost/dfs'
+        self.engine = create_engine(self.db_uri)
+        self.start_urls = ['http://razzball.com/dfsbot-fanduel-pitch/?uid=1505&token=6eef511269ae262c13541abf93953e8e18bd9b7d15b1b9ea74bf4c86c5cb5818&time=1439075726&loadScript=true',
+                'http://razzball.com/dfsbot-fanduel-hit/?uid=1505&token=38c84d51c9e5e3b1a287c39d151ec6c3e6c91881c102655271ec3b67611ea7fd&time=1439085176&loadScript=true']
 
     def parse(self, response):
         header = response.xpath('//div[@id="content"]/article/header/h1/text()').extract()[0].lower()
@@ -37,5 +39,9 @@ class RazzSpider(scrapy.Spider):
                 pts = row.xpath('td[14]/text()').extract()[0]
                 price = row.xpath('td[16]/text()').extract()[0]
 
-            q = ''' INSERT INTO players (name, team, pos, vs_team, points, price) VALUES ('%s', '%s', '%s', '%s', %s, %s) '''
-            self.engine.execute(text(q % (name, team, pos, vsteam, pts, price)))
+            if self.update:
+                q = ''' UPDATE players SET points = %s WHERE name = '%s' AND team = '%s' AND pos = '%s' AND price = %s '''
+                self.engine.execute(text(q % (pts, name, team, pos, price)))
+            else:
+                q = ''' INSERT INTO players (name, team, pos, vs_team, points, price) VALUES ('%s', '%s', '%s', '%s', %s, %s) '''
+                self.engine.execute(text(q % (name, team, pos, vsteam, pts, price)))
